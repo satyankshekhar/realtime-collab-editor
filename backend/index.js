fc97e35a4b1047c03b5fbd7fd6a79ea3b9f8560b
satyankshekhar <satyank.shekhar.11@gmail.com>
2026-01-16 18:31:26 +0530

    Initial project setup: frontend + backend structure

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express();
+
+app.get("/", (req, res) => res.send("Backend running "));
+
+app.listen(5000, () => console.log("Server running on http://localhost:5000"));
